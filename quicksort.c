b77ec38ab3fb4a442cfddd5db7fc1714e54dbc4c
ryan-toddSmacked <94807949+ryan-toddSmacked@users.noreply.github.com>
2022-08-24 14:37:34 -0600

    Update quicksort.c

--- quicksort.c
+++ quicksort.c
@@ -1,23 +1,40 @@
 #include "quicksort.h"
 
-#define M (9)
-#define NSTACK (127)
-static int istack[NSTACK];  /* Auxillary storage */
-#define SWAP(a, b) t = arr[a]; arr[a] = arr[b]; arr[b] = t;
 
-void quicksort (int * restrict arr, int nElems) {
-    int pivotIndex, diff, L, R, i, j, jstack = 0, pivot, t;
-    if (nElems < 2) return;
+// Macros
+#define INSERTION_SORT_SIZE (9)
+#define NSTACK (255)
+#define SWAP(ptr, placeholder, index0, index1) do { \
+    const int _INDEX0 = (index0);    \
+    const int _INDEX1 = (index1);    \
+    (placeholder) = (ptr)[_INDEX0];  \
+    (ptr)[_INDEX0] = (ptr)[_INDEX1]; \
+    (ptr)[_INDEX1] = (placeholder);  \
+} while (0)
+
+// Auxillary storage
+static int istack[NSTACK] = { 0 };
+
+
+int* quicksort (int* arr, int nElems) {
+    // Keep as integers
+    int pivotIndex, diff, L, R, i, j, jstack = 0;
+
+    // These should be the array type being sorted
+    int pivot, t;
+
+
+    if (nElems < 2) return; 
 
     istack[jstack++] = 0;
     istack[jstack++] = nElems - 1;
 
-    for (;;) {
+    while (1) {
         R = istack[--jstack];
         L = istack[--jstack];
         diff = R - L;
-        /* insertion sort if subset array small enough */
-        if (diff < M) {
+        // insertion sort if subset array small enough
+        if (diff < INSERTION_SORT_SIZE) {
             for (i = L + 1; i <= R; i++) {
                 t = arr[i];
                 j = i - 1;
@@ -30,7 +47,7 @@ void quicksort (int * restrict arr, int nElems) {
             if (jstack == 0) break;
         }
         else {
-            SWAP(L, L + (R - L) / 2);
+            SWAP(arr, t, L, L + (R - L) / 2);
             pivot = arr[L];
             pivotIndex = L;
             for (i = R; i > pivotIndex; i--) {
@@ -38,15 +55,19 @@ void quicksort (int * restrict arr, int nElems) {
                     pivotIndex++;
                     while (arr[pivotIndex] <= pivot && pivotIndex < i)
                         pivotIndex++;
-                    SWAP(pivotIndex, i);
+                    SWAP(arr, t, pivotIndex, i);
                 }
             }
-            SWAP(L, pivotIndex);
+            SWAP(arr, t, L, pivotIndex);
+            // To be extra safe, could add if statement to make sure
+            //  jstack wont be larger than NSTACK.
             istack[jstack++] = L;
             istack[jstack++] = pivotIndex - 1;
             istack[jstack++] = pivotIndex + 1;
             istack[jstack++] = R;
         }
     }
+    return arr;
 }
+
 
